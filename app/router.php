3d782a876eda55b4a1b4c3dbaf0e7587d001e710
ryu seunglyul <asimryu@gmail.com>
2016-10-19 11:46:13 +0900

    기본 사이트 구성
    기본 사이트 구성

---
+++ app/router.php
@@ -0,0 +1,18 @@
+<?php
+$url = array("");
+$page = "";
+$file = "";
+if( isset($_GET['url']) ) {
+	$url = explode("/",$_GET['url']);
+}
+if( $url[0] ) {
+	$page = $url[0];
+} else {
+	$page = "home";
+}
+if( $page ) {
+	$file = PAGES . $page . ".php";
+	if( ! file_exists($file) ) {
+		$file = TMPL . "404.php";
+	}
+}
